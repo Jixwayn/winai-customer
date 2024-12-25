92e4ee06eb39c63dc7312358b0cb9c44ccd25420
Jixwayn <27746jiew@gmail.com>
2024-12-25 23:14:42 +0700

    updates

--- สำรอง/หน้าแรก.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {
-    var footer = document.getElementById('footer');
-    // ตรวจสอบว่าเลื่อนถึงล่างสุดหรือไม่
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        footer.style.bottom = '0'; // แสดง footer
-    } else {
-        footer.style.bottom = '-100%'; // ซ่อน footer
-    }
-};
